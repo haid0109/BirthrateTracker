aa5a5590f0225a4128b3ef84b26506a7792de91b
haid0109 <51113567+haid0109@users.noreply.github.com>
2019-09-11 20:36:21 +0200

    Saved SQL Querys

---
+++ GeoTema/UsersDBQuery.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Users 
+(
+	Username NVARCHAR(50) PRIMARY KEY,
+	[Password] NVARCHAR(50) NOT NULL,
+	Usertype NVARCHAR(50) NOT NULL,
+)
+GO
+
+INSERT INTO Users VALUES('1','1','Bruger')
+INSERT INTO Users VALUES('2','2','Superbruger')
+INSERT INTO Users VALUES('3','3','Administrator')
+
+SELECT * FROM Users ORDER BY Usertype ASC, Username ASC
---
+++ GeoTema/fødselsrate_2017Query.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE Africa 
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+CREATE TABLE Asien 
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+CREATE TABLE Europa
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+CREATE TABLE Mellemamerika 
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+CREATE TABLE Nordamerika 
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+CREATE TABLE Oceanien
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+CREATE TABLE Sydamerika
+(
+	Land NVARCHAR(50) PRIMARY KEY,
+	Fødselsrate FLOAT NOT NULL,
+)
+GO
+
+INSERT INTO Africa VALUES('Africa', 1.1)
+INSERT INTO Asien VALUES('Asien', 1.2)
+INSERT INTO Europa VALUES('Europa', 1.3)
+INSERT INTO Mellemamerika VALUES('Mellemamerika', 1.4)
+INSERT INTO Nordamerika VALUES('Nordamerika', 1.5)
+INSERT INTO Oceanien VALUES('Oceanien', 1.6)
+INSERT INTO Sydamerika VALUES('Sydamerika', 1.7)
+
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Africa
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Asien
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Europa
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Mellemamerika
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Nordamerika
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Oceanien
+SELECT *, RANK() OVER (ORDER BY Fødselsrate DESC) AS Rang FROM Sydamerika 
